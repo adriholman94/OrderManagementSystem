12759d369e2ea638fa422f2ad9ae4f52ce131c2a
zaifleitas <zai.fleitas@gmail.com>
2020-12-02 02:29:26 -0300

    .

--- src/main/java/com/fiuni/sd/beans/ProductRegistrationDetail.java
+++ src/main/java/com/fiuni/sd/beans/ProductRegistrationDetail.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
-
+ 
 import org.springframework.data.annotation.Id;
 @Entity
 @Table(name = "productRegistrationDetail")
