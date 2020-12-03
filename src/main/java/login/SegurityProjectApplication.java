4d9689bacef945330a5af085a95ba44b4c7a515d
zaifleitas <zai.fleitas@gmail.com>
2020-12-03 15:41:14 -0300

    login

--- src/main/java/login/SegurityProjectApplication.java
+++ src/main/java/login/SegurityProjectApplication.java
@@ -1,16 +1,22 @@
-package com.fiuni.sd;
+package login;
+
 
 import org.apache.catalina.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-//@Configuration  
-//@Import({ SecurityConfig.class })
+
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Configuration  
+@Import({ SecurityConfig.class })
 @SpringBootApplication
-public class OrderManagementSystemApplication {
+public class SegurityProjectApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(OrderManagementSystemApplication.class, args);
+		SpringApplication.run(SegurityProjectApplication.class, args);
 	}
 
 }
