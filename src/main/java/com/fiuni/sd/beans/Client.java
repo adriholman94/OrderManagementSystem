b4601ec2384eba6984a6580b1ae4676694c7721f
Adrian Holman <adriholman@hotmail.com>
2020-12-03 03:21:07 -0300

    Update Client.java

--- src/main/java/com/fiuni/sd/beans/Client.java
+++ src/main/java/com/fiuni/sd/beans/Client.java
@@ -18,34 +18,34 @@ public class Client implements BaseBean {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "clientId", nullable = false, unique = true)
 	private Integer id;
-	
+
 	@Column(name = "firstName")
 	private String firstName;
-	
+
 	@Column(name = "lastName")
 	private String lastName;
-	
+
 	@Column(name = "clientRuc")
 	private String clientRuc;
-	
+
 	@Column(name = "userPhone")
 	private String clientPhone;
-	
+
 	@OneToOne
-    private User user;
-	
+	private User user;
+
 	public Integer getId() {
 		return id;
 	}
-	
+
 	public String getName() {
 		return firstName;
 	}
-	
+
 	public String getLastName() {
 		return lastName;
 	}
-	
+
 	public String getRUC() {
 		return clientRuc;
 	}
@@ -53,7 +53,7 @@ public class Client implements BaseBean {
 	public String getPhone() {
 		return clientPhone;
 	}
-	
+
 	public User getUser() {
 		return user;
 	}
@@ -61,31 +61,30 @@ public class Client implements BaseBean {
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
 	public void setName(String fristName) {
 		this.firstName = fristName;
 	}
-	
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
-	public void setDocId(String RUC) {
+
+	public void setRUC(String RUC) {
 		this.clientRuc = RUC;
 	}
-	
+
 	public void setPhone(String phone) {
 		this.clientPhone = phone;
 	}
-	
+
 	public void setUser(User user) {
 		this.user = user;
 	}
 
-	public void setEmail(String email) { this.email = email; }
-
 	@Override
 	public String toString() {
-		return "Client [id=" + id + ", name=" + firstName + ", lastName=" + lastName + ", clientRuc=" + clientRuc + ", clientPhone=" + clientPhone + ", user=" + user + "]";
+		return "Client [id=" + id + ", name=" + firstName + ", lastName=" + lastName + ", clientRuc=" + clientRuc
+				+ ", clientPhone=" + clientPhone + ", user=" + user + "]";
 	}
 }
