77a6a59820077e529f485c64a6a276e21e2e0bee
Adrian Holman <adriholman@hotmail.com>
2020-12-11 15:34:38 -0300

    category

--- src/main/java/com/fiuni/sd/Service/Product/ProductService.java
+++ src/main/java/com/fiuni/sd/Service/Product/ProductService.java
@@ -10,8 +10,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.fiuni.sd.Beans.Category.Category;
 import com.fiuni.sd.Beans.Product.Product;
 import com.fiuni.sd.DAO.Product.IProductDAO;
+import com.fiuni.sd.DTO.Category.CategoryDTO;
 import com.fiuni.sd.DTO.Product.ProductDTO;
 import com.fiuni.sd.DTO.Product.ProductResult;
 import com.fiuni.sd.Service.Base.BaseServiceImpl;
@@ -49,15 +51,17 @@ public class ProductService extends BaseServiceImpl<ProductDTO, Product, Product
 		dto.setId(bean.getProductId());
 		dto.setProductName(bean.getProductName());
 		dto.setProductPrice(bean.getProductPrice());
-		dto.setCategory(bean.getCategory());
+		dto.setCategory(convertBeanToDto(bean.getCategory()));
 		return dto;
 	}
 
 	@Override
 	protected Product convertDtoToBean(ProductDTO dto) {
 		final Product bean = new Product();
+		bean.setProductId(dto.getId());
 		bean.setProductName(dto.getProductName());
 		bean.setProductPrice(dto.getProductPrice());
+		bean.setCategory(convertDtoToBean(dto.getCategory()));
 		return bean;
 	}
 
@@ -92,5 +96,21 @@ public class ProductService extends BaseServiceImpl<ProductDTO, Product, Product
 		} else {
 			return null;
 		}
+	}
+
+
+	public CategoryDTO convertBeanToDto(Category bean) {
+		final CategoryDTO DTO = new CategoryDTO();
+		DTO.setId(bean.getId());
+		DTO.setCategoryName(bean.getCategoryName());
+		return DTO;
+	}
+
+
+	protected Category convertDtoToBean(CategoryDTO dto) {
+		final Category bean = new Category();
+		bean.setId(dto.getId());
+		bean.setCategoryName(dto.getCategoryName());
+		return bean;
 	}
 }
