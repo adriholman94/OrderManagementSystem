5f7f31fb2ae1d314616a33d37fc66ac2e456baa0
zaifleitas <zai.fleitas@gmail.com>
2020-12-21 04:20:43 -0300

    update clientDTO

--- src/main/java/com/fiuni/sd/DTO/Client/ClientDTO.java
+++ src/main/java/com/fiuni/sd/DTO/Client/ClientDTO.java
@@ -1,5 +1,6 @@
 package com.fiuni.sd.DTO.Client;
 
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -9,7 +10,39 @@ public class ClientDTO extends BaseDTO {
 	private static final long serialVersionUID = 1L;
 
 	private Integer userId;
+
 	private String clientRuc;
+	private String firstName;
+	private String lastName;
+
+	private String clientPhone;
+	
+	@XmlElement
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	@XmlElement
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	@XmlElement
+	public String getClientPhone() {
+		return clientPhone;
+	}
+
+	public void setClientPhone(String clientPhone) {
+		this.clientPhone = clientPhone;
+	}
+
+
 	@XmlElement
 	public Integer getUserId() {
 		return userId;
