28e28df43e9ca451f7e593d216baa8ec62de5cdb
zaifleitas <zai.fleitas@gmail.com>
2020-12-23 18:55:42 -0300

    role se agrego page

--- src/main/java/com/fiuni/sd/DTO/Role/RoleResult.java
+++ src/main/java/com/fiuni/sd/DTO/Role/RoleResult.java
@@ -11,7 +11,7 @@ import com.fiuni.sd.DTO.Base.BaseResult;
 public class RoleResult extends BaseResult<RoleDTO> {
 
 	private static final long serialVersionUID = 1L;
-
+	private Integer pages;
 	@XmlElement
 	public List<RoleDTO> getRoles() {
 		return list();
@@ -19,5 +19,12 @@ public class RoleResult extends BaseResult<RoleDTO> {
 
 	public void setRoles(List<RoleDTO> dtos) {
 		super.setList(dtos);
+	}
+	public Integer getPages() {
+		return this.pages;
+	}
+
+	public void setPages(Integer pages) {
+		this.pages = pages;
 	}
 }
