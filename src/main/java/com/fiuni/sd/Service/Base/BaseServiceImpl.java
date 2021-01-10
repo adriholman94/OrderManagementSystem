fc4c18484263edcc0b587c27d052710bf6f6c5e6
Adrian Holman <adriholman@hotmail.com>
2021-01-09 23:38:26 -0300

    cache for API

--- src/main/java/com/fiuni/sd/Service/Base/BaseServiceImpl.java
+++ src/main/java/com/fiuni/sd/Service/Base/BaseServiceImpl.java
@@ -4,11 +4,21 @@ import com.fiuni.sd.Beans.Base.BaseBean;
 import com.fiuni.sd.DTO.Base.BaseDTO;
 import com.fiuni.sd.DTO.Base.BaseResult;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+
 
 public abstract class BaseServiceImpl<DTO extends BaseDTO, BEAN extends BaseBean,  RESULT extends BaseResult<DTO>> implements IBaseService<DTO, RESULT> {
 
 	protected abstract DTO convertBeanToDto(BEAN bean);
 
 	protected abstract BEAN convertDtoToBean(DTO dto);
+
+	@Autowired
+	private CacheManager cacheManager;
+
+	protected CacheManager getCacheManager() {
+		return cacheManager;
+	}
 
 }
--- src/main/java/com/fiuni/sd/Service/Category/CategoryService.java
+++ src/main/java/com/fiuni/sd/Service/Category/CategoryService.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,20 +19,34 @@ import com.fiuni.sd.DAO.Category.ICategoryDAO;
 import com.fiuni.sd.DTO.Category.CategoryDTO;
 import com.fiuni.sd.DTO.Category.CategoryResult;
 import com.fiuni.sd.Service.Base.BaseServiceImpl;
+import com.fiuni.sd.Utils.Setting;
 
 @Service
-public class CategoryService extends BaseServiceImpl<CategoryDTO, Category, CategoryResult> implements ICategoryService {
+public class CategoryService extends BaseServiceImpl<CategoryDTO, Category, CategoryResult>
+		implements ICategoryService {
 
 	@Autowired
 	private ICategoryDAO categoryDAO;
 
+	private static Logger logger = LogManager.getLogger(CategoryService.class);
+
 	@Override
 	@Transactional
+	@CachePut(value = Setting.cacheName, key = "'category_' + #dto.id", condition = "#dto.id!=null")
 	public CategoryDTO save(CategoryDTO dto) {
-		final Category bean = new Category();
-		bean.setCategoryName(dto.getCategoryName());
-		final Category role = categoryDAO.save(bean);
-		return convertBeanToDto(role);
+		try {
+			final Category bean = new Category();
+			bean.setCategoryName(dto.getCategoryName());
+			final Category role = categoryDAO.save(bean);
+			final CategoryDTO newDto = convertBeanToDto(role);
+			if (dto.getId() == null) {
+				getCacheManager().getCache(Setting.cacheName).put("category_" + role.getCategoryId(), newDto);
+			}
+			return newDto;
+		} catch (Exception e) {
+			logger.error(e);
+			throw new RuntimeException("Error " + CategoryService.class + " " + e.getMessage(), e);
+		}
 	}
 
 	@Override
@@ -75,6 +92,7 @@ public class CategoryService extends BaseServiceImpl<CategoryDTO, Category, Cate
 
 	@Override
 	@Transactional
+	@Cacheable(value = Setting.cacheName, key = "'category_' + #id")
 	public CategoryDTO deleteById(Integer id) {
 		CategoryDTO dto = new CategoryDTO();
 		if (categoryDAO.existsById(id)) {
@@ -85,6 +103,7 @@ public class CategoryService extends BaseServiceImpl<CategoryDTO, Category, Cate
 	}
 
 	@Override
+	@Cacheable(value = Setting.cacheName, key = "'category_' + #id")
 	public CategoryDTO getById(Integer id) {
 		if (categoryDAO.findById(id).isPresent()) {
 			final Category beans = categoryDAO.findById(id).get();
