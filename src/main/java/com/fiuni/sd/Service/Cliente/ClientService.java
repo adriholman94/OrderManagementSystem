1db8ecc48389011ba2732c15bfa6dd86a193a37b
zai0811 <39562291+zai0811@users.noreply.github.com>
2021-01-14 00:10:40 -0300

    cliente-cache

--- src/main/java/com/fiuni/sd/Service/Cliente/ClientService.java
+++ src/main/java/com/fiuni/sd/Service/Cliente/ClientService.java
@@ -4,18 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
-
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+
 import com.fiuni.sd.Beans.Client.Client;
 import com.fiuni.sd.DAO.Client.IClientDAO;
+
 import com.fiuni.sd.DTO.Client.ClientDTO;
 import com.fiuni.sd.DTO.Client.ClientResult;
 import com.fiuni.sd.Service.Base.BaseServiceImpl;
+import com.fiuni.sd.Service.Category.CategoryService;
+import com.fiuni.sd.Utils.Setting;
 
 @Service
 public class ClientService extends BaseServiceImpl<ClientDTO, Client, ClientResult> implements IClientService {
@@ -23,17 +31,36 @@ public class ClientService extends BaseServiceImpl<ClientDTO, Client, ClientResu
 	@Autowired
 	private IClientDAO clientDAO;
 
+	private static Logger logger = LogManager.getLogger(ClientService.class);
+	
+	@Autowired
+	private CacheManager cacheManager;
+
 	@Override
 	@Transactional
+	//@CacheEvict(value = Setting.cache_Name, key = "'api_client_' + #dto.id")
+	@CachePut(value = Setting.cache_Name, key = "'api_client_' + #dto.id", condition = "#dto.id!=null")
 	public ClientDTO save(ClientDTO dto) {
+		try {
 		final Client bean = new Client();
 		bean.setName(dto.getFirstName());
 		bean.setLastName(dto.getLastName());
 		bean.setRUC(dto.getClientRuc());
 		bean.setPhone(dto.getClientPhone());
 		final Client role = clientDAO.save(bean);
-		return convertBeanToDto(role);
+	
+		final ClientDTO newDto = convertBeanToDto(role);
+		if (dto.getId() == null) {
+			cacheManager.getCache(Setting.cache_Name).put("api_client_" + role.getId(), newDto);
+		}
+		return newDto;
+		}catch (Exception e) {
+		logger.error(e);
+		throw new RuntimeException("Error " + CategoryService.class + " " + e.getMessage(), e);
 	}
+}
+
+	
 
 	@Override
 	@Transactional
@@ -86,6 +113,7 @@ public class ClientService extends BaseServiceImpl<ClientDTO, Client, ClientResu
 
 	@Override
 	@Transactional
+	@CacheEvict(value = Setting.cache_Name, key = "'api_client_' + #id")
 	public ClientDTO deleteById(Integer id) {
 		ClientDTO bean = null;
 		if (clientDAO.existsById(id)) {
@@ -97,6 +125,8 @@ public class ClientService extends BaseServiceImpl<ClientDTO, Client, ClientResu
 	}
 
 	@Override
+	@Transactional
+	@Cacheable(value = Setting.cache_Name, key = "'api_client_' + #id")
 	public ClientDTO getById(Integer id) {
 		if (clientDAO.findById(id).isPresent()) {
 			final Client beans = clientDAO.findById(id).get();
