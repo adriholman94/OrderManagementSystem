a486e852a9be729b51a22d14e60a8947fb66f305
Adrian Holman <adriholman@hotmail.com>
2021-01-21 15:38:38 -0300

    spring security

--- src/main/java/com/fiuni/sd/OrderManagementSystemApplication.java
+++ src/main/java/com/fiuni/sd/OrderManagementSystemApplication.java
@@ -2,11 +2,12 @@ package com.fiuni.sd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @ImportResource({"classpath:xmemcached.xml", "classpath:springSecurity.xml"})
 @EnableWebSecurity
 public class OrderManagementSystemApplication extends SpringBootServletInitializer{
