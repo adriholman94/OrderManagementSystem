ba547f75248fe52283fd93c1f6f7ff693834407d
Adrian Holman <adriholman@hotmail.com>
2021-01-25 00:26:17 -0300

    Update WebSecurityConfig.java

--- src/main/java/com/fiuni/sd/Security/WebSecurityConfig.java
+++ src/main/java/com/fiuni/sd/Security/WebSecurityConfig.java
@@ -29,18 +29,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+
+		http.authorizeRequests().antMatchers("/home", "/login", "/logout").permitAll()
+				.antMatchers("/clients/**", "/products/**", "/orders/**").hasAnyRole("CLIENT", "ADMIN")
+				.antMatchers("/users/**", "purchases/**", "/categories/**", "/roles/**", "/suppliers/**", "/stocks/**")
+				.hasRole("ADMIN").anyRequest().authenticated().and().httpBasic().and().formLogin().loginPage("/login")
+				.defaultSuccessUrl("/home");
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(dataSource)
-				.usersByUsernameQuery(
-						"select user_name, user_password, 1 as enabled " + "from users " + "where user_name = ?")
-				.authoritiesByUsernameQuery(
-						"select u.user_name, r.role_name " + "from users u, users_roles ur, roles r "
-								+ "where u.user_id = ur.user_id and r.role_id = ur.role_id and u.user_name  = ?");
+				.usersByUsernameQuery("select user_name as username, user_password as password, 1 as enabled "
+						+ "from users " + "where user_name = ?")
+				.authoritiesByUsernameQuery("select u.user_name as username, r.role_name as role "
+						+ "from users u, users_roles ur, roles r "
+						+ "where u.user_id = ur.user_id and r.role_id = ur.role_id and u.user_name  = ?");
 	}
 
 	@Bean
