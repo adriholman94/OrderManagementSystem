3030837a4dae8c346564cf4f6705e44157161c43
zai0811 <39562291+zai0811@users.noreply.github.com>
2021-01-28 02:43:07 -0300

    correo

--- src/main/java/com/fiuni/sd/OrderManagementSystemApplication.java
+++ src/main/java/com/fiuni/sd/OrderManagementSystemApplication.java
@@ -1,14 +1,56 @@
 package com.fiuni.sd;
 
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import com.fiuni.sd.Beans.User.User;
 
 @SpringBootApplication
 @ImportResource({"classpath:xmemcached.xml"})
+<<<<<<< Updated upstream
 public class OrderManagementSystemApplication {
 
+=======
+public class OrderManagementSystemApplication extends SpringBootServletInitializer implements CommandLineRunner{
+    @Autowired
+    private JavaMailSender javaMailSender;
+    
+>>>>>>> Stashed changes
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagementSystemApplication.class, args);
 	}
+	
+
+    public void run(String... args) {
+
+        System.out.println("Sending Email...");
+     
+            sendEmail();
+            //sendEmailWithAttachment();
+
+        System.out.println("Done");
+
+    }
+
+    void sendEmail() {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+     //   User b=new User();
+        msg.setTo("zai.fleitas@gmail.com");
+
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText("Hello World \n Spring Boot Email");
+
+        javaMailSender.send(msg);
+
+    }
 }
