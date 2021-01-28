2ac3d67eb9d918b1064555270e19fbeacdf786e7
Adrian Holman <adriholman@hotmail.com>
2021-01-28 13:40:24 -0300

    package

--- src/main/java/com/fiuni/sd/OrderManagementSystemApplication.java
+++ src/main/java/com/fiuni/sd/OrderManagementSystemApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
@@ -14,4 +15,9 @@ public class OrderManagementSystemApplication extends SpringBootServletInitializ
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagementSystemApplication.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(OrderManagementSystemApplication.class);
+    }
 }
